86c2662e7ab97834fb88cb7e104199c988de9c21
Varun-dev10 <varunramteke10@gmail.com>
2026-05-24 20:22:55 +0530

    changed background and added text input

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -3,16 +3,27 @@ import 'package:flutter/material.dart';
 class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
 
+  TimeOfDay get time => TimeOfDay.now();
   @override
   Widget build(BuildContext context) {    /// [BuildContext] tells the flutter frame work that this currency converter widget is found here
                                           ///  It tells the location
     return Scaffold(
-    //  backgroundColor: Color(0xFFD9ED36),
+      backgroundColor: Colors.deepPurple.shade300,   //Colors  has predefined color values ,, Color
       body: Center(
         child: Column(
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('hero'),
+            //Text('$time'),
+            Text('hero',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w500,
+                letterSpacing: 3,
+                color: Colors.white
+              ),
+            ),
+            TextField()
           ],
         ),
       )
