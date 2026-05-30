d3337bc95fa7753dac11718a46e400feedc6b686
Varun-dev10 <varunramteke10@gmail.com>
2026-05-30 16:19:56 +0530

    changed to stateful widget

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,17 +1,22 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget {
+class CurrencyConverterMaterialPage extends StatefulWidget {
   const CurrencyConverterMaterialPage({super.key});
-  //TimeOfDay get time => TimeOfDay.now();
+
+  @override
+  State<CurrencyConverterMaterialPage> createState() =>
+      _CurrencyConverterMaterialPageState();
+}
+
+class _CurrencyConverterMaterialPageState
+    extends State<CurrencyConverterMaterialPage> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     /// [BuildContext] tells the flutter frame work that this currency converter widget is found here
     ///  It tells the location
-
-    double result = 0;
-    final TextEditingController textEditingController = TextEditingController();
 
     const border = OutlineInputBorder(
       borderRadius: BorderRadius.all(Radius.circular(20)),
@@ -81,9 +86,10 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
             ),
 
             //Text('$time'),
-            const Text(
-              'hero', //main hero text
-              style: TextStyle(
+            Text(
+              result
+                  .toString(), //result is object but text widget only accepts string so we convert it to string
+              style: const TextStyle(
                 fontSize: 40,
                 fontWeight: FontWeight.w500,
                 letterSpacing: 3,
@@ -98,7 +104,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                 textAlign: TextAlign.left,
                 style: textstyle, // declare in build to save memory
                 textInputAction:
-                    TextInputAction.go, // action button on keyboard
+                    TextInputAction.none, // action button on keyboard
                 decoration:
                     decoration, //declared in build to define the const declaration of border and decoration
                 keyboardType: TextInputType.number, //  only number input
@@ -114,7 +120,8 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
               child: ElevatedButton(
                 onPressed: () {
                   result = double.parse(textEditingController.text) * 95;
-                }, //on pressed function
+                },
+
                 style: ButtonStyle(
                   backgroundColor: WidgetStateProperty.all(
                     const Color.fromARGB(255, 27, 27, 27),
@@ -157,3 +164,163 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
     );
   }
 }
+
+// class CurrencyConverterMaterialPagee extends StatelessWidget {
+//   const CurrencyConverterMaterialPagee({super.key});
+//   //TimeOfDay get time => TimeOfDay.now();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     /// [BuildContext] tells the flutter frame work that this currency converter widget is found here
+//     ///  It tells the location
+
+//     double result = 0;
+
+//     final TextEditingController textEditingController = TextEditingController();
+
+//     const border = OutlineInputBorder(
+//       borderRadius: BorderRadius.all(Radius.circular(20)),
+//       borderSide: BorderSide(
+//         color: Color.fromARGB(255, 105, 104, 104),
+//         width: 1,
+//       ),
+//     );
+
+//     const decoration = InputDecoration(
+//       //input field decoration
+//       hintText: 'Enter Amount',
+//       hintStyle: TextStyle(color: Color.fromARGB(200, 250, 250, 250)),
+
+//       prefixIcon: Icon(Icons.attach_money),
+//       prefixIconColor: Colors.white,
+//       filled: true,
+//       fillColor: Color.fromARGB(220, 20, 20, 20),
+
+//       focusedBorder:
+//           border, // when the text field is focused, it will show the border defined above
+//       enabledBorder:
+//           border, // when the text field is enabled but not focused, it will show the border defined above
+//     );
+
+//     const textstyle = TextStyle(
+//       color: Colors.white,
+//       fontStyle: FontStyle.italic,
+//       fontSize: 20,
+//     ); //TextStyle
+
+//     return Scaffold(
+//       backgroundColor: const Color.fromARGB(
+//         210,
+//         255,
+//         255,
+//         255,
+//       ), //Colors  has predefined color values ,, Color
+
+//       appBar: AppBar(
+//         title: const Text('Currency Converter'),
+//         centerTitle: true,
+//         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+//         elevation: 10,
+//         toolbarHeight: 45,
+//         shadowColor: const Color.fromARGB(168, 0, 0, 0),
+//         shape: const RoundedRectangleBorder(
+//           borderRadius: BorderRadius.only(
+//             bottomLeft: Radius.circular(15),
+//             bottomRight: Radius.circular(15),
+//           ),
+//         ),
+//       ),
+
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             const Icon(
+//               //circle icon
+//               Icons.circle_outlined,
+//               size: 200,
+//               color: Colors.white,
+//               shadows: [
+//                 Shadow(color: Color.fromARGB(117, 32, 31, 31), blurRadius: 30),
+//               ],
+//             ),
+
+//             //Text('$time'),
+//             Text(
+//               result
+//                   .toString(), //result is object but text widget only accepts string so we convert it to string
+//               style: const TextStyle(
+//                 fontSize: 40,
+//                 fontWeight: FontWeight.w500,
+//                 letterSpacing: 3,
+//                 color: Colors.white,
+//               ),
+//             ),
+//             Padding(
+//               padding: const EdgeInsets.only(left: 30, right: 30, bottom: 50),
+//               child: TextField(
+//                 //input field
+//                 controller: textEditingController, // to control the text field
+//                 textAlign: TextAlign.left,
+//                 style: textstyle, // declare in build to save memory
+//                 textInputAction:
+//                     TextInputAction.none, // action button on keyboard
+//                 decoration:
+//                     decoration, //declared in build to define the const declaration of border and decoration
+//                 keyboardType: TextInputType.number, //  only number input
+//                 cursorHeight: 25,
+//                 cursorColor: Colors.white12, //cursor color
+//                 //showCursor: false,                          // show cursor when text field is focused
+//               ),
+//             ),
+
+//             //button , elevated , text button , outlined button
+//             Padding(
+//               padding: const EdgeInsets.only(top: 20, left: 30, right: 30),
+//               child: ElevatedButton(
+//                 onPressed: () {
+//                   result = double.parse(textEditingController.text) * 95;
+//                 },
+
+//                 style: ButtonStyle(
+//                   backgroundColor: WidgetStateProperty.all(
+//                     const Color.fromARGB(255, 27, 27, 27),
+//                   ),
+//                   side: WidgetStateProperty.all(
+//                     const BorderSide(
+//                       color: Color.fromARGB(255, 255, 255, 255),
+//                       width: 2,
+//                     ),
+//                   ),
+//                   shape: WidgetStateProperty.all(
+//                     RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(20),
+//                     ),
+//                   ),
+//                   minimumSize: WidgetStateProperty.all(
+//                     const Size(
+//                       double.infinity,
+//                       50,
+//                     ), // matches the width pixel for any device
+//                   ),
+//                   elevation: WidgetStateProperty.all(20),
+//                   shadowColor: WidgetStateProperty.all(
+//                     const Color.fromARGB(174, 0, 0, 0),
+//                   ),
+//                 ),
+
+//                 child: const Text(
+//                   'Convert',
+//                   style: TextStyle(
+//                     fontSize: 25,
+//                     color: Color.fromARGB(255, 255, 255, 255),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: CurrencyConverterMaterialPage(),
     );
